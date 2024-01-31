f21d4181086faebe4449ff9747bb7470cff0e360
Bellzie <155555069+Bellzie@users.noreply.github.com>
2024-01-31 16:43:42 +0100

    Update p.js

--- p.js
+++ p.js
@@ -1,10 +1,1 @@
-function Funfacts() {
-    const funfactsElement = document.getElementById('funfacts-info');
-    
-   
-    if (funfactsElement.style.display === 'block') {
-        funfactsElement.style.display = 'none';
-    } else {
-        funfactsElement.style.display = 'block';
-    }
-}
+//bruker ikke//
