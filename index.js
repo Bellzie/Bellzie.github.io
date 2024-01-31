ebfaa00976ee27be3c0f55d17198f787ac607e1b
Bellzie <155555069+Bellzie@users.noreply.github.com>
2024-01-31 16:37:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 function Funfacts() {
+    // Henter referansen til 'funfacts-info'-elementet.
     const funfactsElement = document.getElementById('funfacts-info');
     
-   
+   // Sjekker om 'funfacts-info'-elementet er synlig.
     if (funfactsElement.style.display === 'block') {
+        // Hvis det er synlig, skjul det.
         funfactsElement.style.display = 'none';
+        // Hvis det ikke er synlig, vis det.
     } else {
         funfactsElement.style.display = 'block';
     }
